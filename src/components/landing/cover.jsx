538f6fcdff888e31dc329c662a034e91dff6efcc
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-08-11 16:09:33 +0500

    First Commit

---
+++ src/components/landing/cover.jsx
@@ -0,0 +1,9 @@
+import classes from "./cover.module.css";
+
+export default function Cover() {
+  return (
+    <div className={classes.cover}>
+      <h1>This is For the cover Page</h1>
+    </div>
+  );
+}
